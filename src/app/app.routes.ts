9b41bd6176e476b1ed6e2379d168711b6512d530
kurtruzzelsapo <wkurtruzzelsapo@gmail.com>
2024-11-29 21:38:47 +0800

    adding auth guard and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LandingComponent } from './core/components/landing/landing.component';
 import { SignupStudentComponent } from './core/components/signup.student/signup.student.component';
 import { SignupTeacherComponent } from './core/components/signup.teacher/signup.teacher.component';
 import { LoginComponent } from './core/components/login/login.component';
+import { mainGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [mainGuard] },
   { path: 'signup/student', component: SignupStudentComponent },
   { path: 'signup/teacher', component: SignupTeacherComponent },
 ];
+  

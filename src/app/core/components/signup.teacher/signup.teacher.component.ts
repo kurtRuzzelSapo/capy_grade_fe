62161b94c4490b15f34f1fb105ee76965bef5d1f
kurtruzzelsapo <wkurtruzzelsapo@gmail.com>
2024-11-29 21:13:46 +0800

    Setting up signup for teacher and student

--- src/app/core/components/signup.teacher/signup.teacher.component.ts
+++ src/app/core/components/signup.teacher/signup.teacher.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-signup.teacher',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup.teacher.component.html',
   styleUrl: './signup.teacher.component.css'
 })

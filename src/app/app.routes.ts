69f4bba9be4e8f690563f8dc4dc7b8a40cff9e8d
kurtruzzelsapo <wkurtruzzelsapo@gmail.com>
2024-11-29 21:15:00 +0800

    config and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './core/components/landing/landing.component';
+import { SignupStudentComponent } from './core/components/signup.student/signup.student.component';
+import { SignupTeacherComponent } from './core/components/signup.teacher/signup.teacher.component';
+import { LoginComponent } from './core/components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup/student', component: SignupStudentComponent },
+  { path: 'signup/teacher', component: SignupTeacherComponent },
 ];
